fab497baa3e0fe90d43eae61706c1576e234e56e
Santiagoschzr <151122664+Santiagoschzr@users.noreply.github.com>
2025-12-10 01:34:39 +0200

    PAckagessss

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import SDG3Story from './SDG3Story';
 
 function App() {
     return (
-        // We remove default styles since your component handles its own layout
         <div className="w-full h-full">
             <SDG3Story />
         </div>
